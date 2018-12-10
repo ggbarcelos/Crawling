6d8071327aa1c9ca4f61fe60784c12ea1a52f954
Glauber Gomes Barcelos <glauberbarcelos@hotmail.com>
2018-12-10 17:59:15 -0200

    first commit

---
+++ src/WebCrawling/WebCrawling/Program.cs
@@ -0,0 +1,45 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace WebCrawling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            startCrawlerasync();
+            Console.ReadLine();
+        }
+
+        private static async Task startCrawlerasync()
+        {
+            Startup startup;
+
+            for (int i=1; i<200;i++)
+            {
+                var url = $"https://comunidade.startse.com/perfil/membro/instituicoes/14/{i}";
+                //var url = "https://comunidade.startse.com/startups";
+                var httpClient = new HttpClient();
+                var html = await httpClient.GetStringAsync(url);
+                var htmlDocument = new HtmlDocument();
+                htmlDocument.LoadHtml(html);
+                var divs =
+                    htmlDocument.DocumentNode.Descendants("div")
+                        .Where(node => node.GetAttributeValue("class", "").Equals("bg-white borders mb-20 pf-15 text-center")).ToList();
+                foreach (var div in divs)
+                {
+                    startup = new Startup()
+                    {
+                        UrlImg = div.Descendants("img").FirstOrDefault().ChildAttributes("src").FirstOrDefault().Value,
+                        Nome = div.Descendants("h5").FirstOrDefault().InnerText,
+                        Tipo = div.Descendants("p").FirstOrDefault().InnerText,
+
+                    };
+                }
+            }
+        }
+    }
+}
---
+++ src/WebCrawling/WebCrawling/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebCrawling
+{
+    public class Startup
+    {
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public string Cidade { get; set; }
+        public string UrlImg { get; set; }
+    }
+}
